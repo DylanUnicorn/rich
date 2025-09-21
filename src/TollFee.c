324d6bc3129ddb5aa1ebc37c5d0e73c916a3914d
renjianluosuixin <2583074122@qq.com>
2025-09-21 14:35:15 +0800

    无报错版本

--- src/TollFee.c
+++ src/TollFee.c
@@ -1,6 +1,6 @@
 #include"Player.h"
 #include"Structure.h"
-
+/*
 void GetTollFee(Player *player,Structure *structure,PlayerManager *playermanager){
     if(player->character != structure->owner->character ){
         player->money -= (structure->level+1)*(structure->money)*0.5;
@@ -9,9 +9,11 @@ void GetTollFee(Player *player,Structure *structure,PlayerManager *playermanager
     }//路人过路费为（房产等级+1）乘以基础金额乘以0.5
 }
 
+
 void IBankruptcy(Player *player,PlayerManager *playermanager){
     if(player->money<0){
-        /*命令行显示破产函数于此处*/
+        //命令行显示破产函数于此处
         playerManager_removeBankruptPlayer(playermanager,player->character);
     }
 }
+*/
