a95c83976b04a02fd750b741318883d595ec33d7
Shelly <1602785329@qq.com>
2025-09-21 09:59:05 +0800

    颜色色板

---
+++ include/Color.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define COLOR_RESET   "\033[0m"
+#define COLOR_RED     "\033[31m"
+#define COLOR_GREEN   "\033[32m"
+#define COLOR_YELLOW  "\033[33m"
+#define COLOR_BLUE    "\033[34m"
+#define COLOR_MAGENTA "\033[35m"
+#define COLOR_CYAN    "\033[36m"
+#define COLOR_WHITE   "\033[37m"
+
+// 玩家颜色
+static inline const char* getPlayerColor(char code) {
+    switch (code) {
+        case 'Q': return COLOR_RED;
+        case 'A': return COLOR_GREEN;
+        case 'S': return COLOR_YELLOW;
+        case 'J': return COLOR_BLUE;
+        default:  return COLOR_WHITE;
+    }
+}
